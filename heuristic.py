07aaceccea245abad5c5ae260aa0027088ea4518
Kevin <kevxemail@gmail.com>
2024-01-03 17:30:15 -0700

    obtain Chicago Train Authority datasets and wrote Great Circle Distance heuristic function

---
+++ heuristic.py
@@ -0,0 +1,19 @@
+from math import pi , acos , sin , cos
+
+# There are a series of longitudes and latitudes from the Chicago Train Authority's dataset.
+# Use GREAT CIRCLE DISTANCE formula for heuristic for A* since the trains aren't traveling on a flat plane, earth is a sphere
+def calcd(node1, node2):
+   # y1 = lat1, x1 = long1
+   # y2 = lat2, x2 = long2
+   # all assumed to be in decimal degrees
+   y1, x1 = node1
+   y2, x2 = node2
+
+   R   = 3958.76 # miles = 6371 km, this is the radius of earth
+   y1 *= pi/180.0
+   x1 *= pi/180.0
+   y2 *= pi/180.0
+   x2 *= pi/180.0
+
+   # approximate great circle distance with law of cosines
+   return acos( sin(y1)*sin(y2) + cos(y1)*cos(y2)*cos(x2-x1) ) * R
